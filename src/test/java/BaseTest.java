8b881832561a6f4aed1ad82b6daa12ec9b1bb08b
rekswami <rekha.swaminathan@testpro.io>
2025-03-05 20:25:13 -0800

    Adding Song to Playlist Homework17
    To add Song to newly created Playlist Homework 17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,61 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+import java.time.Duration;
 
-    @BeforeSuite
-    static void setupClass() {
+public class BaseTest {
+  public WebDriver driver;
+  public String Url = "https://qa.koel.app/";
+  @BeforeSuite
+  static void setupClass()
+    {
         WebDriverManager.chromedriver().setup();
     }
+
+  @BeforeMethod
+  public void launchClass()
+   {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+    driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.manage().window().maximize();
+    navigateToPage();
+  }
+  public void navigateToPage()
+  {
+   driver.get(Url);
+   }
+@AfterMethod
+public void closeBrowser()
+{
+    driver.quit();
+}
+public void provideEmail(String email)
+{
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys(email);
+}
+public void providePassword(String password)
+{
+    WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+    passwordField.clear();
+    passwordField.sendKeys(password);
+}
+public void clickLoginBtn()
+{
+    WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+    submit.click();
+ }
+
+
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,61 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+import static org.openqa.selenium.By.*;
+
+public class Homework17 extends BaseTest {
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+
+        String expectedSongAddedMessage = "Added 1 song into \"Playlist Homework17.\"";
+        navigateToPage();
+        provideEmail("rekha.swaminathan@testpro.io");
+        providePassword("PHL0a6FA");
+        clickLoginBtn();
+        Thread.sleep(2000);
+        searchSong("Lament");
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
+        choosePlaylist();
+        Assert.assertEquals(getAddToPlaylistSuccessMsg(), expectedSongAddedMessage);
+         }
+
+        public void searchSong (String name) throws InterruptedException {
+            WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+            searchField.sendKeys(name);
+            Thread.sleep(2000);
+        }
+        public void clickViewAllBtn () throws InterruptedException {
+            WebElement viewAll = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+            viewAll.click();
+            Thread.sleep(2000);
+        }
+        public void selectFirstSongResult () throws InterruptedException {
+            WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+            firstSong.click();
+            Thread.sleep(2000);
+        }
+
+        public void clickAddToBtn () throws InterruptedException {
+            WebElement addToButton = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@class='btn-add-to']"));
+            addToButton.click();
+            Thread.sleep(2000);
+        }
+        public void choosePlaylist () throws InterruptedException {
+            WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Playlist Homework17')]"));
+            playlist.click();
+            Thread.sleep(2000);
+        }
+
+        public String getAddToPlaylistSuccessMsg()  {
+            WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+            return notification.getText();
+            }
+
+    }
+
