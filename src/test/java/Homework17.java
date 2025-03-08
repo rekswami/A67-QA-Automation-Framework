69f3d070f61b1f653ac55eef070ca7debb7a4209
rekswami <rekha.swaminathan@testpro.io>
2025-03-08 15:25:31 -0800

    Add Song to Playlist Homework17
    Added Song "Pluto" to Playlist Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -17,7 +17,7 @@ public class Homework17 extends BaseTest {
         providePassword("PHL0a6FA");
         clickLoginBtn();
         Thread.sleep(2000);
-        searchSong("Episode 2");
+        searchSong("Pluto");
         clickViewAllBtn();
         selectFirstSongResult();
         clickAddToBtn();
