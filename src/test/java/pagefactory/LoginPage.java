79990836403d92cd525b915fb91319be020ba255
rekswami <rekha.swaminathan@testpro.io>
2025-04-16 22:38:37 -0700

    Parallel Testing
    Parallel Testing Implementation with ThreadLocal Usage to run test in Cloud(With  Lamba Test account creation)

---
+++ src/test/java/pagefactory/LoginPage.java
@@ -0,0 +1,31 @@
+package pagefactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+        @FindBy(css = "input[type='email']")
+        WebElement emailField;
+        @FindBy(css = "input[type='password']")
+        WebElement passwordField;
+        @FindBy(css = "button[type='submit']")
+        WebElement submit;
+     public LoginPage(WebDriver givenDriver){
+            super(givenDriver);
+        }
+
+    public LoginPage provideEmail(String email){
+        emailField.sendKeys(email);
+        return this;
+    }
+    public LoginPage providePassword(String password){
+        passwordField.sendKeys(password);
+        return this;
+    }
+
+    public void submitBtn(){
+        submit.click();
+    }
+}
