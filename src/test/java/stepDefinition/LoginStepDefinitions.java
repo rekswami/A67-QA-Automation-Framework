2e61ca7c66df0a409daa44e081471382a99119fa
rekswami <rekha.swaminathan@testpro.io>
2025-04-20 22:15:39 -0700

    Cucumber
    Installed Cucumber and Implemented test case for Positive Login Test Scenario

---
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -0,0 +1,62 @@
+package StepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class LoginStepDefinitions {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Given("I open browser")
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    @And("I open Login page")
+    public void openLoginPage() {
+        driver.get("https://qa.koel.app/");
+    }
+
+    @When("I enter email {string}")
+    public void i_enter_email(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys(email);
+
+    }
+
+    @And("I enter password {string}")
+    public void i_enter_password(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
+    }
+    @And("I click submit")
+    public void iClickSubmit() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']"))).click();
+    }
+    @Then("I am logged in")
+    public void userIsLoggedIn() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']"))).isDisplayed());
+    }
+
+    @After
+    public void closebrowser(){
+        driver.quit();
+    }
+
+}
